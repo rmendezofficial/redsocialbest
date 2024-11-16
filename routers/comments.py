bca9a19e1532a5b7927b30068b867a7342a82a08
rmendezofficial <ricgabmendezdiscord@gmail.com>
2024-11-16 11:06:40 -0500

    Update comments.py

--- routers/comments.py
+++ routers/comments.py
@@ -11,6 +11,7 @@ from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
 from jose import jwt,JWTError
 from passlib.context import CryptContext
 from datetime import datetime,timedelta
+from .users import current_user
 
 router=APIRouter(prefix='/comments',responses={404:{'message':'No encontrado'}})
 
@@ -27,23 +28,40 @@ class CommentBase(BaseModel):
     comment:str
     
 @router.post('/create_comment',status_code=status.HTTP_201_CREATED)
-async def create_comment(comment:CommentBase,db:Session=Depends(get_db)):
-    db_comment=Comments(**comment.model_dump())
-    db.add(db_comment)
-    db.commit()
-    return{'message':'Comment successfuly created'}
+async def create_comment(request:Request,comment:CommentBase,db:Session=Depends(get_db),user_auth:Users=Depends(current_user)):
+    user=db.query(Users).filter(Users.id==comment.user_id).first()
+    csrf_token_db=user.token
+    csrf_token_req=request.cookies.get('csrf_token')
+    if csrf_token_db==csrf_token_req:
+        db_comment=Comments(**comment.model_dump())
+        db.add(db_comment)
+        db.commit()
+        return{'message':'Comment successfuly created'}
+    return {'message':'CSRF FAILED'}
 
 @router.put('/update_comment',status_code=status.HTTP_200_OK)
-async def update_comment(comment_id:int,comment:CommentBase,db:Session=Depends(get_db)):
-    comment_db=db.query(Comments).filter(Comments.id==comment_id).first()
-    comment_db.comment=comment.comment
-    comment_db.edited=True
-    db.commit()
-    return {'message':'Comment succesfuly updated'}
+async def update_comment(request:Request,comment_id:int,comment:CommentBase,db:Session=Depends(get_db),user_auth:Users=Depends(current_user)):
+    user=db.query(Users).filter(Users.id==comment.user_id).first()
+    csrf_token_db=user.token
+    csrf_token_req=request.cookies.get('csrf_token')
+    if csrf_token_db==csrf_token_req:
+        comment_db=db.query(Comments).filter(Comments.id==comment_id).first()
+        comment_db.comment=comment.comment
+        comment_db.edited=True
+        db.commit()
+        return {'message':'Comment succesfuly updated'}
+    return {'message':'CSRF FAILED'}
 
-@router.delete('/delete_user/{comment_id}')
-async def delete_comment(comment_id:int,db:Session=Depends(get_db)):
+@router.delete('/delete_comment/{comment_id}')
+async def delete_comment(request:Request,comment_id:int,db:Session=Depends(get_db),user_auth:Users=Depends(current_user)):
     comment_db=db.query(Comments).filter(Comments.id==comment_id).first()
-    db.delete(comment_db)
-    db.commit()
-    return {'message':'Comment succesfuly deleted'}
+    user=db.query(Users).filter(Users.id==comment_db.user_id).first()
+    csrf_token_db=user.token
+    csrf_token_req=request.cookies.get('csrf_token')
+    if csrf_token_db==csrf_token_req:
+        db.delete(comment_db)
+        db.commit()
+        return {'message':'Comment succesfuly deleted'}
+    return {'message':'CSRF FAILED'}
+
+
